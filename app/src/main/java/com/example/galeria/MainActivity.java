600d12333be3e47c627a958064e077f95cf7e52e
Isabelly Eduarda Buge <isabellyeduardabuge@gmail.com>
2024-06-18 08:59:44 -0300

    atividade dia 18 de junho

--- app/src/main/java/com/example/galeria/MainActivity.java
+++ app/src/main/java/com/example/galeria/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.galeria;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -12,8 +13,16 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    List<String> photos = new ArrayList<>();
+    MainAdapter mainAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +38,21 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File[] files = dir.listFiles();
+        for(int i = 0; i < files.length; i++) {
+            photos.add(files[i].getAbsolutePath());
+        }
+
+        mainAdapter = new MainAdapter(MainActivity.this, photos);
+        RecyclerView rvGallery = findViewById(R.id.rvGallery);
+        rvGallery.setAdapter(mainAdapter);
+
+        float w = getResources().getDimension(R.dimen.itemWidth);
+        int numberOfColumns = Util.calculateNoOfColumns(MainActivity.this, w);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(MainActivity.this, numberOfColumns);
+        rvGallery.setLayoutManager(gridLayoutManager);
     }
     @Override
     public boolean onCreateOptionsMenu (Menu menu){
---
+++ app/src/main/java/com/example/galeria/MainAdapter.java
@@ -0,0 +1,50 @@
+package com.example.galeria;
+
+import android.graphics.Bitmap;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class MainAdapter extends RecyclerView.Adapter {
+    MainActivity mainActivity;
+    List<String> photos;
+    public MainAdapter(MainActivity mainActivity, List<String> photos) {
+        this.mainActivity = mainActivity;
+        this.photos = photos;
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(mainActivity);//obtemos um inflador de layouts
+        View v = inflater.inflate(R.layout.list_item,parent,false);//cria os elementos de interface referentes a um item usando o inflador e guardamos dentro da View
+        return new MyViewHolder(v);//retorna a View atraves da funcao
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, final int position) {
+        ImageView imPhoto = holder.itemView.findViewById(R.id.imItem);
+        int w = (int) mainActivity.getResources().getDimension(R.dimen.itemWidth);
+        int h = (int) mainActivity.getResources().getDimension(R.dimen.itemHeight);
+        Bitmap bitmap = Util.getBitmap(photos.get(position), w, h);
+        imPhoto.setImageBitmap(bitmap);
+        imPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mainActivity.startPhotoActivity(photos.get(position));
+            }
+        });
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return photos.size();
+    }
+}
