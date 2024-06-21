d3fe99601e6b020ede4a9b4dc83c1dfa7ef2ebfe
Isabelly Eduarda Buge <isabellyeduardabuge@gmail.com>
2024-06-21 11:01:12 -0300

    atividade dia 21 de junho

--- app/src/main/java/com/example/galeria/PhotoActivity.java
+++ app/src/main/java/com/example/galeria/PhotoActivity.java
@@ -1,26 +1,42 @@
 package com.example.galeria;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.FileProvider;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.File;
+
 public class PhotoActivity extends AppCompatActivity {
+
+    String photoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_photo);
+
+        Intent i = getIntent();
+        photoPath = i.getStringExtra("photo_path");
+
+        Bitmap bitmap = Util.getBitmap(photoPath);
+        ImageView imPhoto = findViewById(R.id.imPhoto);
+        imPhoto.setImageBitmap(bitmap);
 
         Toolbar toolbar = findViewById(R.id.tbPhoto);
         setSupportActionBar(toolbar);
@@ -45,12 +61,19 @@ public class PhotoActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected (@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.opShare:
+        if (item.getItemId() == R.id.opShare{
                 sharePhoto();
                 return true;
-            default:
-                return super.onOptionsItemSelected(item);
         }
+        return super.onOptionsItemSelected(item);
+    }
+
+    void sharePhoto() {
+        // Codigo para cpmpartiilhar a foto
+        Uri photoUri = FileProvider.getUriForFile(PhotoActivity.this,"trindade.daniel.galeria.fileprovider", new File(photoPath));
+        Intent i = new Intent(Intent.ACTION_SEND);
+        i.putExtra(Intent.EXTRA_STREAM, photoUri);
+        i.setType("image/jpeg");
+        startActivity(i);
     }
 }
